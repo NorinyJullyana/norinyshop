009454cfa61e209bde68218a09968a462427e498
noriny jullyana <noriny.pessoa@gmail.com>
2024-03-06 04:44:21 -0300

    components add

---
+++ src/components/clienteCadastro.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h1>Cadastro Cliente</h1>
+    </div>
+
+  </template>
+  
+  <script>
+  export default {
+    name: 'HelloWorld',
+    
+  }
+  </script>
+  
+  <style scoped>
+ 
+  </style>
+  

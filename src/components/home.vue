2d8102312bb71010d30eefebd1ddcb2a525a809f
noriny jullyana <noriny.pessoa@gmail.com>
2024-03-07 23:21:16 -0300

    home

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,22 +1,57 @@
 <template>
-    <div>
-      <h1 class="titulo">Home</h1>
+  <div >
+    <div class="projeto-info">
+      <h1 class="titulo">Bem-vindo ao Sistema de Cadastro de Clientes e Associação de Produtos</h1>
+      <h2>Objetivo do Projeto:</h2>
+      <p>
+        Construir um sistema de cadastro de clientes e associação de produtos oferecidos aos mesmos.
+      </p>
+      
+      <h2>Telas do Sistema:</h2>
+      <ul>
+        <li>Tela de Cadastro de Produtos com os campos (nome, ativo [Sim ou Não]).</li>
+        <li>Tela de Cadastro de Clientes com os campos (nome, documento, telefone, e-mail, ativo [Sim ou Não]).</li>
+        <li>Tela para Associar Produtos ao Cliente, exibindo o nome do cliente e os produtos disponíveis para associação.</li>
+      </ul>
+      
+      <h2>Finalidade:</h2>
+      <p>
+        Disponibilizar para a área comercial um cadastro de clientes e uma forma de associar os produtos que cada cliente tem disponível para utilizar na plataforma.
+      </p>
+      
+      <h2>Listagens:</h2>
+      <ul>
+        <li>Listagem de produtos cadastrados com opções para editar, inativar/ativar.</li>
+        <li>Listagem de todos os clientes com opções de edição, inativar/ativar e associar produtos.</li>
+      </ul>
     </div>
+  </div>
+</template>
 
-  </template>
-  
-  <script>
-  export default {
-    name: 'home-page',
-    
-  }
-  </script>
-  
-  <style scoped>
+<script>
+export default {
+  name: 'home-page',
+}
+</script>
 
-  .titulo{
-    font-weight: 700;
-  }
- 
-  </style>
-  
+<style scoped>
+.titulo {
+  font-weight: 700;
+}
+
+.projeto-info {
+  margin-top: 20px;
+}
+
+.projeto-info h2 {
+  margin-bottom: 10px;
+}
+
+.projeto-info p {
+  margin-bottom: 15px;
+}
+</style>
+
+.container {
+  flex-direction: column;
+}
